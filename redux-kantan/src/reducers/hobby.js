a85b9d9e3947c83a6dd9530031e351211b1096b4
17021084 <dotrunghg1999@gmail.com>
2020-05-23 15:26:06 +0700

    create reducers

---
+++ redux-kantan/src/reducers/hobby.js
@@ -0,0 +1,5 @@
+const hobbyReducer =(state,action )=>{
+    return state;
+}
+
+export default hobbyReducer;
---
+++ redux-kantan/src/reducers/index.js
@@ -0,0 +1,12 @@
+import { hobbyReducer } from "./hobby";
+import { userReducer } from "./user";
+import { combineReducers } from "redux";
+
+//Manage all reducers of App
+
+const rootReducer = combineReducers({
+  hobby: hobbyReducer,
+  user: userReducer,
+});
+
+export default rootReducer;
